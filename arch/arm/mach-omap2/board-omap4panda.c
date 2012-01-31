19e17c0d9e0442ed80b3051051042f2fb91bd87e
Dan Murphy <dmurphy@ti.com>
2012-01-31 07:30:19 -0600

    ARM: OMAP4: Remove forced headset detection
    Need to figure out the coexistance between other audio channls and the
    wired headset
    
    Change-Id: Ic82539c7871dfd0541da04ab567abf1b25c071e2
    Signed-off-by: Dan Murphy <dmurphy@ti.com>

--- arch/arm/mach-omap2/board-omap4panda.c
+++ arch/arm/mach-omap2/board-omap4panda.c
@@ -463,7 +463,6 @@ static struct twl4030_codec_audio_data twl6040_audio = {
 	.hf_left_step	= 0x1d,
 	.hf_right_step	= 0x1d,
 	.hs_switch_dev  = 0x1,
-	.hs_forced_hs_state = 0x1
 };
 
 static struct twl4030_codec_data twl6040_codec = {
